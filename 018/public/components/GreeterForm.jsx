01b7f34734f1586a2be6a024f8d50ef322a22e91
Marek Chowaniok <marek.chowaniok@gmail.com>
2016-11-21 13:53:25 -0500

    Finished React Greeter

---
+++ 018/public/components/GreeterForm.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+
+var GreeterForm = React.createClass({
+    onFormSubmit: function (e) {
+        e.preventDefault();
+        var name = this.refs.name.value;
+        var message = this.refs.message.value;
+        var updates = {};
+
+        if (name.length > 0) {
+            this.refs.name.value = '';
+            updates.name = name;
+        }
+        if (message.length > 0) {
+            this.refs.message.value = '';
+            updates.message = message;
+        }
+        this.props.onNewData(updates);
+    },
+
+    render: function () {
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                    <div><input type="text" ref="name" placeholder="Enter name"/></div>
+                    <div><textarea placeholder="Enter message" ref="message"/></div>
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+});
+
+
+module.exports = GreeterForm;
