6a7385a7dbc7e0aa12782cd018783cb7ae93e5b7
Marek Chowaniok <marek.chowaniok@gmail.com>
2016-11-23 14:47:07 -0500

    ReactWeather app

---
+++ ReactWeather/ReactWeather/app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var objOne = {
+    name: 'Marek',
+    locataion: 'JAX'
+};
+
+var objTwo = {
+    age: 25,
+    ...objOne
+};
+
+console.log(objTwo);
+
+ReactDOM.render(
+    <h1>Boilerplate app</h1>, document.getElementById('app')
+);
